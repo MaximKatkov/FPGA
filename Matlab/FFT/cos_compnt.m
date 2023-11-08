d802a1eb6846c6c50154e6d76b3a89c40df6aaee
Katkov Maxim <mkatkov355@gmail.com>
2023-11-08 22:11:40 +0300

    simple update

--- Matlab/FFT/cos_compnt.m
+++ Matlab/FFT/cos_compnt.m
@@ -1,5 +1,6 @@
-function c = cos_compnt(x, n, m, N, Pi)
-    
-    c = x * ( cos( (2*Pi*n*m) / N ) ); 
+function c = cos_compnt(x, n, m, N)
+
+    Pi = 3.141592653589793;
+    c = x * ( cos( (2*Pi*m*n) / N ) ); 
 
     
--- Matlab/FFT/sin_compnt.m
+++ Matlab/FFT/sin_compnt.m
@@ -1,3 +1,4 @@
-function s = sin_compnt(x, n, m, N, Pi)
+function s = sin_compnt(x, n, m, N)
 
-    s = x * ( sin( (2*Pi*n*m) / N ) );
+    Pi = 3.141592653589793;
+    s = x * ( sin( (2*Pi*m*n) / N ) );
